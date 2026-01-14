7e42cd993436f502af64b71f856d147c04f03a89
MorenoBarrosoJorge <125257908+MorenoBarrosoJorge@users.noreply.github.com>
2026-01-14 12:18:29 +0100

    Modelo "Mision"
    CORRECIÓN: En el anterior commit, el modelo creado fue "Mision", no "Nivel"

--- app/src/main/java/com/example/betalifeup/presentation/creator/options/custom/Custom2ViewModel.kt
+++ app/src/main/java/com/example/betalifeup/presentation/creator/options/custom/Custom2ViewModel.kt
@@ -1,1 +1,7 @@
 package com.example.betalifeup.presentation.creator.options.custom
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.example.betalifeup.presentation.model.Nivel
+import com.example.betalifeup.presentation.model.Mision
