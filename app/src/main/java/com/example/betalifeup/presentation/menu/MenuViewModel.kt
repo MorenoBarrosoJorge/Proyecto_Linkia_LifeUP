68f24371d722a66a2cb38462cf935231110fd126
MorenoBarrosoJorge <125257908+MorenoBarrosoJorge@users.noreply.github.com>
2026-01-17 11:33:57 +0100

    MenuViewModel/ Actualización cargarMetas
    Se ha actualizado la forma en la que se escuchaban las metas del usuario. Ahora está contenido en la función cargarMetas()

--- app/src/main/java/com/example/betalifeup/presentation/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/betalifeup/presentation/menu/MenuViewModel.kt
@@ -6,18 +6,24 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.betalifeup.presentation.model.Meta
 import com.example.betalifeup.data.MetaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MenuViewModel(
     private val repository: MetaRepository = MetaRepository()
 ) : ViewModel() { // Hereda de ViewModel (librería interna de Android) que permite que la pantalla "sobreviva" a recomposiciones de Compose o rotación de pantalla
 
-    var metas by mutableStateOf<List<Pair<String, Meta>>>(emptyList())
-        private set // La UI puede leer las metas, pero no escribirlas
+    private val _metas = MutableStateFlow<List<Meta>>(emptyList()) // Lista de metas mutable
+    val metas: StateFlow<List<Meta>> = _metas // Lista de metas inmutable
 
-    init { // Cuando se crea el ViewModel, es decir, cuando la lógica de la pantalla comienza a funcionar...
-        repository.escucharMetas( // Se llama a la función responsable de recoger cada una de las metas asignadas al usuario actual
-            onResult = { metas = it }, // Se asigna la lista de metas a la variable "metas" (que se recogerá en MenuScreen.kt)
-            onError = { /* manejar error */ }
-        )
+    init {
+        fun cargarMetas() {
+            repository.escucharMetas(
+                onResult = { listaMetas -> // Se recogen todas las metas del usuario
+                    _metas.value = listaMetas
+                },
+                onError = {/*Controlar error al escuchar metas*/ }
+            )
+        }
     }
 }
